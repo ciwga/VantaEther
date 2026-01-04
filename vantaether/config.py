040637dbaf8299d7c0b200837180156f6f91e5c5
ciwga <42353725+ciwga@users.noreply.github.com>
2026-01-04 13:54:54 +0300

    feat(config): add application versioning and localized ASCII banner

---
+++ vantaether/config.py
@@ -0,0 +1,26 @@
+from typing import Final
+from vantaether.utils.i18n import LanguageManager
+
+
+lang = LanguageManager()
+
+
+VERSION: Final[str] = "1.0"
+
+BANNER: Final[str] = rf"""
+[bold white]██╗   ██╗ █████╗ ███╗   ██╗████████╗ █████╗[/]
+[bold white]██║   ██║██╔══██╗████╗  ██║╚══██╔══╝██╔══██╗[/]
+[bold white]██║   ██║███████║██╔██╗ ██║   ██║   ███████║[/]
+[bold white]╚██╗ ██╔╝██╔══██║██║╚██╗██║   ██║   ██╔══██║[/]
+[bold white] ╚████╔╝ ██║  ██║██║ ╚████║   ██║   ██║  ██║[/]
+[bold white]  ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝[/]
+           [bold white on #007acc] V A N T A [/][bold black on white] E T H E R [/] [bold cyan]v{VERSION}[/]
+       [dim]━━━ [italic]{lang.get('app_description')}[/] ━━━[/]
+"""
+
+# ███████╗████████╗██╗  ██╗███████╗██████╗ 
+# ██╔════╝╚══██╔══╝██║  ██║██╔════╝██╔══██╗
+# █████╗     ██║   ███████║█████╗  ██████╔╝
+# ██╔══╝     ██║   ██╔══██║██╔══╝  ██╔══██╗
+# ███████╗   ██║   ██║  ██║███████╗██║  ██║
+# ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
